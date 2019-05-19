2d709b75e719d2d57f253a96f7d530336d1de73d
Jinthebandit <50792847+Jinthebandit@users.noreply.github.com>
2019-05-19 11:26:44 +0200

    Update kamera.py

--- theshaker/common/kamera.py
+++ theshaker/common/kamera.py
@@ -9,55 +9,45 @@ from skimage.measure import compare_ssim
 
 # Local Imports
 
-class kamera:
-	def calibrate(self,msg):
-		camera = PiCamera()
-		camera.resolution = (640,480)
+def calibrate(self,msg):
+	camera = PiCamera()
+	camera.resolution = (640,480)
 
-		rawCapture1 = PiRGBArray(camera)
-		time.sleep(0.5)
+	rawCapture1 = PiRGBArray(camera)
+	time.sleep(0.5)
 
-		camera.capture(rawCapture1, format="bgr")
-		calibrate = rawCapture1.array
+	camera.capture(rawCapture1, format="bgr")
+	calibrate = rawCapture1.array
 
-		cv2.imwrite("../Pictures/calibrate.jpg", calibrate)
-		camera.close()
+	cv2.imwrite("../resources/calibrate.jpg", calibrate)
+	camera.close()
 
-	def compare(self,msg):
-		camera = PiCamera()
-		camera.resolution = (640,480)
+def compare(self,msg):
+	camera = PiCamera()
+	camera.resolution = (640,480)
 
-		rawCapture2 = PiRGBArray(camera)
-		time.sleep(0.5)
+	rawCapture2 = PiRGBArray(camera)
+	time.sleep(0.5)
 
-		camera.capture(rawCapture2, format="bgr")
-		compare = rawCapture2.array
+	camera.capture(rawCapture2, format="bgr")
+	compare = rawCapture2.array
 
-		calibrate = cv2.imread("../Pictures/calibrate.jpg")
+	calibrate = cv2.imread("../resources/calibrate.jpg")
 
-		roiA = calibrate[130:460, 140:600]
-		roiB = compare[130:460, 140:600]
+	roiA = calibrate[130:460, 140:600]
+	roiB = compare[130:460, 140:600]
 
-		grayA = cv2.cvtColor(roiA, cv2.COLOR_BGR2GRAY)
-#		grayA = cv2.GaussianBlur(grayA, (5,5), 0)
+	grayA = cv2.cvtColor(roiA, cv2.COLOR_BGR2GRAY)
+	grayA = cv2.GaussianBlur(grayA, (5,5), 0)
 
-		grayB = cv2.cvtColor(roiB, cv2.COLOR_BGR2GRAY)
-#		grayB = cv2.GaussianBlur(grayB, (5,5), 0)
+	grayB = cv2.cvtColor(roiB, cv2.COLOR_BGR2GRAY)
+	grayB = cv2.GaussianBlur(grayB, (5,5), 0)
 
-		(score, diff) = compare_ssim(grayA, grayB, full=True)
-		diff = (diff * 255).astype("uint8")
+	(score, diff) = compare_ssim(grayA, grayB, full=True)
+	diff = (diff * 255).astype("uint8")
 
-		cv2.imwrite("../Pictures/difference.jpg", diff)
+	cv2.imwrite("../resources/difference.jpg", diff)
 
-		camera.close()
+	camera.close()
 
-		percent = 100*score
-		if score >= 0.6:
-			print("Uebereinstimmung: " + str(percent) + "Prozent")
-			print("mehr steine")
-		else:
-			print("Form ist voll: Programm beenden")
-
-#camera.calibrate(" "," ")
-#time.sleep(1)
-#camera.compare(" "," ")
+	return score
