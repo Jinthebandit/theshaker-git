e11ef07574f25d607fe6b5b8a856f9aa6c4fe707
Jinthebandit <50792847+Jinthebandit@users.noreply.github.com>
2019-05-19 20:52:41 +0200

    Update dc.py

--- theshaker/common/dc.py
+++ theshaker/common/dc.py
@@ -1,5 +1,13 @@
 #! /common/dc.py python3
 
 # Library Imports
+import time
+import piplates.MOTORplate as MOTOR
+import paho.mqtt.client as mqtt
 
 # Local Imports
+from ..data.settings import config
+
+class dc:
+  def start(self,msg):
+    print("start dc")
