e5c50cf0b94ad5990dc3caa162877903a70e4911
Jinthebandit <50792847+Jinthebandit@users.noreply.github.com>
2019-05-19 11:21:52 +0200

    Update settings.py

--- theshaker/data/settings.py
+++ theshaker/data/settings.py
@@ -1,7 +1,13 @@
-#! /Projects/config.py python3
+#! /data/config.py python3
 
-# ---- MQTT Konfiguration ----
+# ---- MQTT Settings ----
 BROKER = "localhost"
 PORT = 1883
 CHANNEL = "pdp"
 TOPICS = {"stepper": "stepper", "camera": "camera"}
+
+# ---- /MQTT Settings ----
+
+# ---- Stepper Settings ----
+
+# ---- /Stepper Settings ----
