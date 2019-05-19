603f068f7e016497855b47cecf476d87f073214b
Jinthebandit <50792847+Jinthebandit@users.noreply.github.com>
2019-05-19 12:19:17 +0200

    Update kamera.py

--- theshaker/common/kamera.py
+++ theshaker/common/kamera.py
@@ -9,49 +9,50 @@ from skimage.measure import compare_ssim
 
 # Local Imports
 
-# Erstellt Foto zur Kalibrierung und speichert es zur spaeteren Verwendung
-def calibrate(self,msg):
-	camera = PiCamera()
-	camera.resolution = (640,480)
+class kamera:
+	# Erstellt Foto zur Kalibrierung und speichert es zur spaeteren Verwendung
+	def calibrate(self,msg):
+		camera = PiCamera()
+		camera.resolution = (640,480)
 
-	rawCapture1 = PiRGBArray(camera)
-	time.sleep(0.5)
+		rawCapture1 = PiRGBArray(camera)
+		time.sleep(0.5)
 
-	camera.capture(rawCapture1, format="bgr")
-	calibrate = rawCapture1.array
+		camera.capture(rawCapture1, format="bgr")
+		calibrate = rawCapture1.array
 
-	cv2.imwrite("../resources/calibrate.jpg", calibrate)
-	camera.close()
+		cv2.imwrite("../resources/calibrate.jpg", calibrate)
+		camera.close()
 
-# Ladet das Kalibrierungsfoto und vergleicht es mit einer neuen Aufnahme
-def compare(self,msg):
-	camera = PiCamera()
-	camera.resolution = (640,480)
+	# Ladet das Kalibrierungsfoto und vergleicht es mit einer neuen Aufnahme
+	def compare(self,msg):
+		camera = PiCamera()
+		camera.resolution = (640,480)
 
-	rawCapture2 = PiRGBArray(camera)
-	time.sleep(0.5)
+		rawCapture2 = PiRGBArray(camera)
+		time.sleep(0.5)
 
-	camera.capture(rawCapture2, format="bgr")
-	compare = rawCapture2.array
+		camera.capture(rawCapture2, format="bgr")
+		compare = rawCapture2.array
 
-	calibrate = cv2.imread("../resources/calibrate.jpg")
+		calibrate = cv2.imread("../resources/calibrate.jpg")
 	
-	# Beschraenkt die area of interest auf die Form
-	roiA = calibrate[130:460, 140:600]
-	roiB = compare[130:460, 140:600]
+		# Beschraenkt die area of interest auf die Form
+		roiA = calibrate[130:460, 140:600]
+		roiB = compare[130:460, 140:600]
 
-	# Farbanpassung beider Aufnahmen
-	grayA = cv2.cvtColor(roiA, cv2.COLOR_BGR2GRAY)
-	grayA = cv2.GaussianBlur(grayA, (5,5), 0)
-	grayB = cv2.cvtColor(roiB, cv2.COLOR_BGR2GRAY)
-	grayB = cv2.GaussianBlur(grayB, (5,5), 0)
+		# Farbanpassung beider Aufnahmen
+		grayA = cv2.cvtColor(roiA, cv2.COLOR_BGR2GRAY)
+		grayA = cv2.GaussianBlur(grayA, (5,5), 0)
+		grayB = cv2.cvtColor(roiB, cv2.COLOR_BGR2GRAY)
+		grayB = cv2.GaussianBlur(grayB, (5,5), 0)
 
-	# Vergleich der Aufnahmen und Ausgabe der Veraenderung in Grautoenen
-	(score, diff) = compare_ssim(grayA, grayB, full=True)
-	diff = (diff * 255).astype("uint8")
+		# Vergleich der Aufnahmen und Ausgabe der Veraenderung in Grautoenen
+		(score, diff) = compare_ssim(grayA, grayB, full=True)
+		diff = (diff * 255).astype("uint8")
 	
-	# Vergleichsbild speichern und Kamera schliessen
-	cv2.imwrite("../resources/difference.jpg", diff)
-	camera.close()
+		# Vergleichsbild speichern und Kamera schliessen
+		cv2.imwrite("../resources/difference.jpg", diff)
+		camera.close()
 
-	return score
+		return score
