64ce208d15f61c74fe34208d57950ebdad89c442
Jinthebandit <50792847+Jinthebandit@users.noreply.github.com>
2019-05-20 17:37:58 +0200

    Update programm.py

--- theshaker/common/programm.py
+++ theshaker/common/programm.py
@@ -13,6 +13,7 @@ client.connect(config.BROKER,config.PORT,60)
 
 class prg:
     def prg1(self,msg):
+        print("prg1")
         client.publish("pdp/stepper", json.dumps({ "mode": "calibrate" }))
         client.publish("pdp/kamera", json.dumps({ "mode": "calibrate" }))
         client.publish("pdp/stepper", json.dumps({ "mode": "move", "dir": "cw", "steps": 40 }))
