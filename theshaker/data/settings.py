e8151f57a366ac8b82e470c5060523a5a40df5b1
Jinthebandit <jinthebandit@gmail.com>
2019-05-20 22:16:04 +0200

    refactored

--- theshaker/data/settings.py
+++ theshaker/data/settings.py
@@ -1,16 +1,17 @@
 #! /data/settings.py python3
 
-class config:
-  # ---- MQTT Settings ----
-  BROKER = "localhost"
-  PORT = 1883
-  CHANNEL = "pdp"
-  TOPICS = [ "stepper", "kamera", "dc", "servo", "prg" ]
-  # ---- /MQTT Settings ----
 
-  # ---- Stepper Settings ----
-  ADDR = 0
-  MOTOR = "A"
-  RES = "M8"
-  SPEED = 500
-  # ---- /Stepper Settings ----
+class config:
+    # ---- MQTT Settings ----
+    BROKER = "localhost"
+    PORT = 1883
+    CHANNEL = "pdp"
+    TOPICS = ["stepper", "kamera", "dc", "servo", "prg"]
+    # ---- /MQTT Settings ----
+
+    # ---- Stepper Settings ----
+    ADDR = 0
+    MOTOR = "A"
+    RES = "M8"
+    SPEED = 500
+    # ---- /Stepper Settings ----
