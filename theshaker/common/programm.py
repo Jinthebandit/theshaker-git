9d0226f2458550b8734fb5322347c2bc1cd12417
Jinthebandit <50792847+Jinthebandit@users.noreply.github.com>
2019-05-20 17:28:28 +0200

    Update programm.py

--- theshaker/common/programm.py
+++ theshaker/common/programm.py
@@ -2,10 +2,20 @@
 
 # Library Imports
 import time
+import json
 import paho.mqtt.client as mqtt
 
 # Local Imports
+from ..data.settings import config
+
+client = mqtt.Client("")
+client.connect(config.BROKER,config.PORT,60)
 
 class prg:
   def prg1(self,msg):
-    
+    client.publish("pdp/stepper", json.dumps({ "mode": "calibrate" })
+    time.sleep(0.5)
+    client.publish("pdp/kamera", json.dumps({ "mode": "calibrate" })
+    client.publish("pdp/stepper", json.dumps({ "mode": "move", "dir": "cw", "steps": 40 })
+    client.publish("pdp/kamera", json.dumps({ "mode": "compare" })
+    print("fertig")
