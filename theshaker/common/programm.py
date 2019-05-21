6ead9f099592109e2a70935f07da244957051a0f
Jinthebandit <jinthebandit@gmail.com>
2019-05-21 11:41:32 +0200

    use load instead of score refactored

--- theshaker/common/programm.py
+++ theshaker/common/programm.py
@@ -15,21 +15,21 @@ client.connect(config.BROKER,config.PORT,60)
 
 class prg:
     def __init__(self):
-        print("Programme")
+        print('Programme')
 
-    def prg2(self,msg):
+    def prg2(self, msg):
         stepper().calibrate(1)
         kamera().calibrate(1)
         time.sleep(5)
         kamera().compare(1)
         time.sleep(5)
-        stepper().move({ "dir": "cw", "steps": 40 })
+        stepper().move({ 'dir': 'cw', 'steps': 40 })
         kamera().compare(1)
         time.sleep(5)        
-        stepper().move({ "dir": "ccw", "steps": 40 })
-        score = kamera().compare(1)
-        if score >= 0.6:
-            print("mehr Steine: " + str(score))
+        stepper().move({ "dir": 'ccw', 'steps': 40 })
+        load = kamera().compare(1)
+        if load <= 95:
+            print('mehr Steine: {}'.format(load))
         else:
-            print(str(score))
+            print(load)
         stepper().off(1)
