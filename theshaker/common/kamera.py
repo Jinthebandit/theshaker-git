e59dc6b4b3dc32aaae4223fb4403d74d1bf153d3
Jinthebandit <jinthebandit@gmail.com>
2019-05-21 11:29:49 +0200

    precent reworked

--- theshaker/common/kamera.py
+++ theshaker/common/kamera.py
@@ -63,7 +63,7 @@ class kamera:
         camera.close()
 
         # Send MQTT message containing the difference in percent and the load status of the form
-        percent = round((score-0.286) * 142)
+        percent = round((score-0.27) * 139)
         load = 100 - int(percent)
         client = mqtt.Client('')
         client.connect(config.BROKER, config.PORT, 60)
